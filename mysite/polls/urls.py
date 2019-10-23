e20d2fcdee247a8a575197762313c6d5f73f75a9
ily <michal.sklyar@p3-group.com>
2019-10-23 16:12:44 +0200

    Without goupby

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('data_get', views.data_get, name='data'),
-    path('data_post', views.data_post, name='data'),
-    path('upload_data_file', views.upload_data_file, name='data')
+    path('data_post', views.data_post, name='data_post'),
+    path('data_get_specific', views.data_get_specific, name='data_get_specific'),
+    path('upload_data_file', views.upload_data_file, name='upload_data_file')
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,16 +2,53 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from polls.models import DataSet
 from django.views.decorators.csrf import csrf_exempt
+from django.db.models import Sum, Count, Avg, Q, Case, F, FloatField, When
 import json
 import csv
 import os
 
+data_query = None
+
 
 def data_get(request):
     if request.method == 'GET':
-        try:
-            # p = Data.objects.get(first_field=first_field)
+        global data_query
+
+        if not data_query:
             data_query = DataSet.objects.all()
+
+        args = {'data': data_query}
+        return render(request, 'detail.html', args)
+
+
+@csrf_exempt
+def data_get_specific(request):
+    global data_query
+    if request.method == 'POST':
+        try:
+            json_data = json.loads(request.body)
+            group_by = json_data['group_by'] if 'group_by' in json_data.keys() else ''
+            order_by = json_data['sort_by'] if 'sort_by' in json_data.keys() else ''
+            filter_by = json_data['filter_by'] if 'filter_by' in json_data.keys() else ''
+            filters = {}
+            for key in filter_by.keys():
+                val = filter_by[key]
+                if val:
+                    if key == 'date_from':
+                        key = 'date__gte'
+                    if key == 'date_to':
+                        key = 'date__lte'
+                    filters[key] = val
+            data_query = DataSet.objects.values('country', 'channel', 'date').filter(**filters).annotate(
+                at_total_instance=F("installs"),
+                CPI=Case(
+                    When(at_total_instance=0, then=0),
+                    default=F("spend") / F("installs"),
+                    output_field=FloatField()
+                ))
+            if order_by:
+                args = []
+                data_query = data_query.order_by(*args)[:-1]
 
         except DataSet.DoesNotExist:
             raise Http404("Poll does not exist")
@@ -41,19 +78,16 @@ def upload_data_file(request):
         with open(os.path.join(os.getcwd(), r'polls\dataset.csv')) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             line_count = 0
+            col_names = list()
             for row in csv_reader:
                 if line_count == 0:
                     print(f'Column names are {", ".join(row)}')
+                    col_names = row
                     line_count += 1
                 else:
-                    d = DataSet(date=row[0],
-                                channel=row[1],
-                                country=row[2],
-                                os=row[3],
-                                impressions=row[4],
-                                clicks=row[5],
-                                installs=row[6],
-                                spend=row[7],
-                                revenue=row[8])
+                    dict_values = {}
+                    for i in range(len(row)):
+                        dict_values[col_names[i]] = row[i]
+                    d = DataSet(**dict_values)
                     d.save()
     return HttpResponse("Updated data")
